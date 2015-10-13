de3718767b218b5c9206adc418ec4b58def1d750
Rares Dragus <rares.dragus@softvision.ro>
2015-10-13 18:36:01 +0300

    Updating user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -6,9 +8,6 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-  end
-
-  def index
   end
 
   def create
@@ -21,4 +20,28 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = 'Profile updated'
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def signed_in_user
+      redirect_to signin_url, notice: 'Please sign in.' unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
+    end
 end
