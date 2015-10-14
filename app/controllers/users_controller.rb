baa0785df8cde0c0a7f3c9fe03c39708243a6cbe
Rares Dragus <rares.dragus@softvision.ro>
2015-10-14 11:56:00 +0300

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -13,8 +18,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      sign_in @user
       flash[:success] = 'Welcome to the Sample App!'
+      sign_in @user
       redirect_to @user
     else
       render 'new'
@@ -34,14 +39,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
   private
 
-    def signed_in_user
-      redirect_to signin_url, notice: 'Please sign in.' unless signed_in?
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: 'Please sign in.'
     end
+  end
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to root_url unless current_user?(@user)
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
 end
