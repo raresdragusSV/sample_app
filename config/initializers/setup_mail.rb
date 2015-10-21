5167730002bf843d16545fd377d019213a1d4a67
Rares Dragus <rares.dragus@softvision.ro>
2015-10-21 12:52:33 +0300

    Heroku Confirmation Email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -8,10 +8,10 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "localhost:3000",
   :user_name            => "rares.dragus@gmail.com",
-  :password             => "secret",
+  :password             => "Cal1psoUl1se",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
