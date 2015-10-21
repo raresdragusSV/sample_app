8e1a72a030be573f185291c5e790efd01ca9255e
Rares Dragus <rares.dragus@softvision.ro>
2015-10-21 15:23:44 +0300

    Reset mailer_setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "localhost:3000",
   :user_name            => "rares.dragus@gmail.com",
-  :password             => "Cal1psoUl1se",
+  :password             => "secret",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
